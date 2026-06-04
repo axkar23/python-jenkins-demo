f50e2d078108c72a8a697a1baf1ed1d11a2df144
axkar23 <askaralialiaskar982@gmail.com>
2026-06-04 18:00:00 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from DevOps askar Project 🚀"})
+    return jsonify({"message": "Hello from DevOps askar ali Project 🚀"})
 
 @app.route('/health')
 def health():
